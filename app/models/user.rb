1f4fa1e66e4099380ea9b4505ff732d5e23f2a82
hankaholla <holla.anicka@gmail.com>
2023-01-06 00:15:15 +0100

    Create new db migration to work with user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,7 +10,8 @@ class User < ApplicationRecord
   has_many :poly_likes
 
   validates :user_name, presence: true
-  validates :password, presence: true, length: { minimum: 5 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_171612) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_222245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_171612) do
     t.string "first_name"
     t.string "last_name"
     t.string "user_name"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
