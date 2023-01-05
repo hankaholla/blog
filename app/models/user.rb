bc443940121379995c587c71d1e39a0b3a7fd9d7
hankaholla <holla.anicka@gmail.com>
2023-01-05 22:14:01 +0100

    Add user registration through devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
     
-    has_many :comments
-    has_many :articles
-    has_many :poly_likes
+  has_many :comments
+  has_many :articles
+  has_many :poly_likes
 
-    validates :user_name, presence: true
-    validates :password, presence: true, length: { minimum: 5 }
+  validates :user_name, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :email, presence: true
     
 end
