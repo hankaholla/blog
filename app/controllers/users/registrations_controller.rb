84ce53081ba316ba4db8dfb06b47df7238b37c0f
hankaholla <holla.anicka@gmail.com>
2023-01-22 23:32:31 +0100

    Refactoring, adding more bootstrap

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-  end
-
-  # POST /resource
-  def create
-    super
-  end
+  # def new
+  #   super
+  # end
+  #
+  # # POST /resource
+  # def create
+  #   super
+  # end
+  #
+  # def show
+  #   super
+  # end
 
   # GET /resource/edit
   # def edit
@@ -41,9 +45,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :first_name, :last_name])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :first_name, :last_name])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
